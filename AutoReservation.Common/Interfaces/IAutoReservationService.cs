24c498d992e7ba46aa1d16d64f189f86d5c2c652
blubiszirp <frank-jordi@hotmail.com>
2015-11-16 16:25:40 +0100

    implement IAutoReservationService

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,69 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract] // TODO Namespace?
     public interface IAutoReservationService
     {
-        
+        /**
+         * Auto 
+         */
+
+        [OperationContract]
+        IList<AutoDto> Autos();
+
+        [OperationContract]
+        AutoDto GetAuto(int id);
+
+        [OperationContract]
+        void InsertAuto(AutoDto auto);
+
+        [OperationContract]
+        void UpdateAuto(AutoDto modified, AutoDto original);
+
+        [OperationContract]
+        void DeleteAuto(AutoDto auto);
+
+
+        /**
+         * Kunde
+         */
+
+        [OperationContract]
+        IList<KundeDto> Kunden();
+
+        [OperationContract]
+        KundeDto GetKunde(int id);
+
+        [OperationContract]
+        void InsertKunde(KundeDto kunde);
+
+        [OperationContract]
+        void UpdateKunde(KundeDto modified, KundeDto original);
+
+        [OperationContract]
+        void DeleteKunde(KundeDto kunde);
+
+        /**
+         * Reservation
+        */
+
+        [OperationContract]
+        IList<ReservationDto> Reservationen();
+
+        [OperationContract]
+        ReservationDto GetReservation(int id);
+
+        [OperationContract]
+        void InsertReservation(ReservationDto reservation);
+
+        [OperationContract]
+        void UpdateReservation(ReservationDto modified, ReservationDto original);
+
+        [OperationContract]
+        void DeleteReservation(ReservationDto auto);
+
     }
 }
